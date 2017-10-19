76a2f0aa6dac9da52bba0b15b738daa4cba06a2f
Matthew Carr <Matthew.Carr2@cerner.com>
2017-10-19 15:20:04 -0500

    Initial push

---
+++ spec/folder-notes-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import FolderNotesView from '../lib/folder-notes-view';
+
+describe('FolderNotesView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
